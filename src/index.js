1ca9bff3e6e215f6e42db249e33ac036ddc68131
yangshi <yangshi@sensorsdata.cn>
2020-04-15 11:19:17 +0800

    feat: dva api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import dva from 'dva';
 import './index.css';
+import example from './models/example'
 
 // 1. Initialize
-const app = dva();
+// const app = dva();
+
+const app = dva({
+  initialState: {
+    products: [
+      { name: 'cz', id: 1 },
+      { name: 'ys', id: 2 },
+    ]
+  }
+})
 
 // 2. Plugins
 // app.use({});
 
 // 3. Model
 // app.model(require('./models/example').default);
+app.model(example)
+app.model(require('./models/products').default);
 
 // 4. Router
 app.router(require('./router').default);
 
+
+
+console.log(app)
 // 5. Start
 app.start('#root');
--- src/routes/DetailPage.js
+++ src/routes/DetailPage.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'dva';
+import ProductList from '../components/Products';
 
-const Detail = (props) => (
-  <h2>detail page</h2>
-);
+const Products = (store) => {
+  console.log('????', store)
+  const {dispatch, products} = store
+  function handleDelete(id) {
+    dispatch({
+      type: 'products/delete',
+      payload: id,
+    });
+  }
+  return (
+    <div>
+      <h2>List of Products</h2>
+      <ProductList onDelete={handleDelete} products={products} />
+    </div>
+  );
+};
 
-export default Detail;
+// export default Products;
+export default connect((input) => {
+  console.log('connect', input)
+  return {
+    products: input.products,
+  }
+})(Products);
